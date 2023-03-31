b855dbbec61b25c32bdc3b50d3458c32ee1d3ebe
Zackary Oon <zackaryoon134@berkeley.edu>
2023-03-31 11:10:09 -0700

    Finish 1.6 good, neutral, bad buttons and count

---
+++ Part_1_React_Intro/ex_1.6-11_unicafe/src/App.js
@@ -0,0 +1,30 @@
+import {useState} from 'react';
+
+const Header = ({header}) => <h1>{header}</h1>;
+
+const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>;
+
+const Stats = ({feedback, count}) => <p>{feedback} {count}</p>;
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+
+  return (
+    <div>
+      <Header header="give feedback" />
+      <Button handleClick={() => setGood(good + 1)} text='good' />
+      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
+      <Button handleClick={() => setBad(bad + 1)} text='bad' />
+      <Header header='statistics'/>
+      <Stats feedback='good' count={good} />
+      <Stats feedback='neutral' count={neutral} />
+      <Stats feedback='bad' count={bad} />
+
+    </div>
+  )
+};
+
+export default App;
+  
