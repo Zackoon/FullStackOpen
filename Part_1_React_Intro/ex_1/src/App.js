3b05c4a589ec5c3bda170be9cc4655d8b1e9fa88
Zackary Oon <zackaryoon134@berkeley.edu>
2023-03-30 23:41:06 -0700

    App.js contains an example of simple state usage

--- Part_1_React_Intro/ex_1/src/App.js
+++ Part_1_React_Intro/ex_1/src/App.js
@@ -1,72 +1,45 @@
-const Header = (props) => {
-  console.log(props)
-  return (
-    <>
-      <h1>{props.course}</h1>
-    </>
-  )
-}
+import {useState} from 'react'
 
-const Content = (props) => {
-  const part1 = props.parts[0];
-  const part2 = props.parts[1];
-  const part3 = props.parts[2];
+const Display = ({ counter }) => <div>{counter}</div>
+  
+const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
 
-  return (
-    <div>
-      <Part partName={part1.name} exNum={part1.exercises}/>
-      <Part partName={part2.name} exNum={part2.exercises}/>
-      <Part partName={part3.name} exNum={part3.exercises}/>
-    </div>
-  )
-}
-
-const Total = (props) => {
-  const part1 = props.parts[0];
-  const part2 = props.parts[1];
-  const part3 = props.parts[2];
-
-  return (
-    <>
-      <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
-    </>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <>
-      <p>{props.partName}  {props.exNum}</p>
-    </>
-  )
-}
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const [counter, setCounter] = useState(0)
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => { 
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
   }
 
   return (
     <div>
-      <Header course={course.name}/>
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
+      <Display counter={counter} />
+      <Button 
+        handleClick={increaseByOne}
+        text='plus'
+      />
+      <Button 
+        handleClick={setToZero}
+        text='zero'
+      />
+      <Button 
+        handleClick={decreaseByOne}
+        text='minus'
+      />
     </div>
   )
 }
-
 export default App;
