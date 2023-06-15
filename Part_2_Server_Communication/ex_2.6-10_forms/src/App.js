2db96739eb99cf112e71513486ec3d126f70a460
Zackary Oon <zackaryoon134@berkeley.edu>
2023-06-15 00:01:38 -0700

    Finish 2.10 -- code refactor into components complete

--- Part_2_Server_Communication/ex_2.6-10_forms/src/App.js
+++ Part_2_Server_Communication/ex_2.6-10_forms/src/App.js
@@ -1,23 +1,9 @@
 import { useState } from 'react'
 
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+// refactor into Search, AddNew, People -> Person within that
 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [newSearch, setNewSearch] = useState('')
-  const [searchedPersons, setSearchedPersons] = useState([])
-
-  const handleChange = (setState) => {
-    //try doing "" vs newName for value of input
-    return (event) => setState(event.target.value)
-  }
-
+const Search = ({persons, newSearch,    setNewSearch, searchedPersons, setSearchedPersons}) => {
+  
   const handleSearch = (event) => {
     const checkName = (person) => {
       return person.name.toLowerCase().includes(event.target.value.toLowerCase())
@@ -26,14 +12,31 @@ const App = () => {
     setSearchedPersons(persons.filter(checkName))
   }
 
-  // a way to mimic the logic of "includes" but using part of the object rather than the whole object
-  const nameExists = (name) => {
-    return persons.some((person) => person.name === name)
+  return (
+    <div>
+        filter shown with: <input value={newSearch} onChange={handleSearch}/>
+    </div>
+  )
+}
+
+const AddNew = ({persons, setPersons}) => {
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  // for dealing with input changes in the form
+  const handleChange = (setState) => {
+    //try doing "" vs newName for value of input
+    return (event) => setState(event.target.value)
   }
 
   const addPerson = (event) => {
     event.preventDefault()
-
+    
+    // a way to mimic the logic of "includes" but using part of the object rather than the whole object
+    const nameExists = (name) => {
+      return persons.some((person) => person.name === name)
+    }
+  
     if (nameExists(newName)) {
       alert(`${newName} is already added to the phonebook`)
       return 
@@ -44,19 +47,8 @@ const App = () => {
     setNewNumber('')
   }
 
-  const addPhoneNum = (event) => {
-    event.preventDefault()
-
-
-  }
-
   return (
-    <div>
-      <h2>Phonebook</h2>
-      <div>
-        filter shown with: <input value={newSearch} onChange={handleSearch}/>
-      </div>
-      <form onSubmit={addPerson}>
+    <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} 
                       onChange={handleChange(setNewName)}/>
@@ -68,17 +60,61 @@ const App = () => {
         <div>
           <button type="submit">add</button>
         </div>
-      </form>
+    </form>
+  )
+}
+
+const People = ({persons, searchedPersons, newSearch}) => {
+  return (
+  <div>
+    {newSearch == "" 
+        ? persons.map(person => <Person person={person} key={person.name}/>) 
+        : searchedPersons.map(person => <Person person={person} key={person.name}/>) 
+    }
+  </div>
+  )
+}
+
+const Person = ({person}) => <div key={person.name}>{person.name} {person.number}</div>
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
+  const [newSearch, setNewSearch] = useState('')
+  const [searchedPersons, setSearchedPersons] = useState([])
+
+  
+  /*
+  const handleSearch = (event) => {
+    const checkName = (person) => {
+      return person.name.toLowerCase().includes(event.target.value.toLowerCase())
+    }
+    setNewSearch(event.target.value)
+    setSearchedPersons(persons.filter(checkName))
+  }*/
+
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Search persons={persons} 
+              newSearch={newSearch} 
+              setNewSearch={setNewSearch}
+              searchedPersons={searchedPersons}
+              setSearchedPersons={setSearchedPersons}/> 
+
+      <h2>Add a New Member</h2>
+      <AddNew persons={persons} setPersons={setPersons}/>
+
       <h2>Numbers</h2>
-      {newSearch == "" 
-        ? 
-        persons.map(person => 
-        <div key={person.name}>{person.name} {person.number}</div>) 
-        : 
-        searchedPersons.map(person => 
-          <div key={person.name}>{person.name} {person.number}</div>)
-       }
-      
+      <People persons={persons} 
+              searchedPersons={searchedPersons}
+              newSearch={newSearch}/>
     </div>
   )
 }
